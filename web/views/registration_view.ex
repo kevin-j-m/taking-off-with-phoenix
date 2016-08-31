c133b77abc5d8b40e0f9e7ebed3f9b17c5062324
Kevin Murphy <murphy.kevin.mail@gmail.com>
2016-08-31 17:00:52 -0400

    Log in/log out

---
+++ web/views/registration_view.ex
@@ -0,0 +1,11 @@
+defmodule Workshop.RegistrationView do
+  use Workshop.Web, :view
+
+  # def render("new.json", _assigns) do
+  #   %{hello: "world"}
+  # end
+
+  # def render("new.html", _assigns) do
+  #   ~E"<h1>Hello world</h1>"
+  # end
+end
